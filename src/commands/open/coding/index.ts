17880ca277e4df79b68b83a27dff54c766ca95fd
Yuns <yuns.xie@qq.com>
2024-08-19 11:04:37 +0800

    🐛 fix: return empty if parse coding error

--- src/commands/open/coding/index.ts
+++ src/commands/open/coding/index.ts
@@ -3,15 +3,21 @@ import os from 'node:os'
 import { getCurrentRepoUrl } from '../../../helpers/git'
 import { getCurrentWorkspace } from '../../../helpers/workspaces'
 import { logger } from '../../../utils'
+import { getErrorMessage } from '../../../helpers/errors'
 
 import { getCodingRepoLinks, parseCodingRepoUrl } from './helpers'
 
 export async function getCodingRepoResources() {
-  const currentWorkSpace = await getCurrentWorkspace()
-  const repoUrl = await getCurrentRepoUrl(os.platform() === 'win32' ? `file://${currentWorkSpace}` : currentWorkSpace)
+  try {
+    const currentWorkSpace = await getCurrentWorkspace()
+    const repoUrl = await getCurrentRepoUrl(os.platform() === 'win32' ? `file://${currentWorkSpace}` : currentWorkSpace)
 
-  const { repo, project, team } = parseCodingRepoUrl(repoUrl)
-  logger.info('Parsed CODING repo', JSON.stringify({ repo, project, team }))
+    const { repo, project, team } = parseCodingRepoUrl(repoUrl)
+    logger.info('Parsed CODING repo', JSON.stringify({ repo, project, team }))
 
-  return getCodingRepoLinks(team, project, repo)
+    return getCodingRepoLinks(team, project, repo)
+  } catch (err) {
+    logger.error(getErrorMessage(err))
+  }
+  return []
 }
